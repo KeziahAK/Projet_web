e16fcd03c5fa3157fb0743c5be240350b5f202b0
KeziahAK <kez.kgs@gmail.com>
2023-02-14 18:00:39 +0100

    TP1

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,26 +1,22 @@
 <template>
   <div class="books-gallery">
-      <BookCard/>
-  </div> 
-  
+      <h1> Hunger Games </h1>
+      <p>je sais plus le nom de l'auteur</p>
+  </div>
 </template>
 
 <script>
 
-import BookCard from './components/BookCard.vue'
 
 export default {
-  name: 'App',
-  components:{
-    BookCard
-  }
+  name: 'BookCard'
+
 }
 
 </script>
 
 <style>
 #app {
-  background-color: blue;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: left;
   color: #adf8a4;
